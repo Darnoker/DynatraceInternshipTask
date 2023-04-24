06a5d84c84abb2288570fbf9573f5a0b5ba7a5b4
Konrad <Darnok10000@gmail.com>
2023-04-24 21:35:02 +0200

    update MaxAndMinRate

--- src/main/java/internship/task/dynatracetask/data/MaxAndMinRate.java
+++ src/main/java/internship/task/dynatracetask/data/MaxAndMinRate.java
@@ -1,8 +1,12 @@
 package internship.task.dynatracetask.data;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
+@EqualsAndHashCode
 public class MaxAndMinRate {
 
     private double max;
